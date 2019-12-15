427ed5aee63d54bdab7fbe5ae03bbd25b297ff01
Mony Kaushik <kaushikmony17@gmail.com>
2019-12-15 22:47:49 +1100

    completed exercise 1.3

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -3,18 +3,24 @@ import ReactDOM from 'react-dom'
 
 const App = () => {
     const course = 'Half Stack application development'
-    const part1 = 'Fundamentals of React'
-    const exercises1 = 10
-    const part2 = 'Using props to pass data'
-    const exercises2 = 7
-    const part3 = 'State of a component'
-    const exercises3 = 14
+    const part1 = {
+        name: 'Fundamentals of React',
+        exercises: 10
+    }
+    const part2 = {
+        name: 'Using props to pass data',
+        exercises: 7
+    }
+    const part3 = {
+        name: 'State of a component',
+        exercises: 14
+    }
 
     return (
         <div>
             <Header course={course}/>
-            <Content part1 = {part1} exercise1={exercises1} part2 = {part2} exercise2={exercises2} part3 = {part3} exercise3={exercises3}/>
-            <Footer exercise1={exercises1} exercise2={exercises2} exercise3 ={exercises3}/>
+            <Content part1 = {part1} part2 = {part2}  part3 = {part3} />
+            <Footer exercise1={part1.exercises} exercise2={part2.exercises} exercise3 ={part3.exercises}/>
         </div>
     )
 }
@@ -22,17 +28,17 @@ const App = () => {
 const Header = ({course})=> <h1>{course}</h1>
 
 const Content = (props) => {
+    console.log(props)
     return(
         <div>
-            <Part part ={props.part1} value ={props.exercise1}/>
-            <Part part ={props.part2} value ={props.exercise2}/>
-            <Part part ={props.part3} value ={props.exercise3}/>
+            <Part name ={props.part1.name} exercises={props.part1.exercises} />
+            <Part name ={props.part2.name} exercises={props.part2.exercises} />
+            <Part name ={props.part3.name} exercises={props.part3.exercises} />
         </div>
     )
 }
 
 const Footer = ({exercise1,exercise2,exercise3})=>{
-
     return(
         <div>
             Number of exercises {exercise1+exercise2+exercise3}
@@ -40,10 +46,11 @@ const Footer = ({exercise1,exercise2,exercise3})=>{
     )
 }
 
-const Part = ({part,value})=>{
+const Part = ({name,exercises})=>{
+    console.log(name,exercises)
     return(
         <div>
-            {part} {value}
+            {name} {exercises}
         </div>
     )
 }
