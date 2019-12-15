cd5e0a7c8e517ff47e66db2af2566a0128606848
Mk <mk@Mks-MacBook-Air.local>
2019-12-16 00:21:50 +1100

    completed exercise 1.6 and 1.7

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,6 +1,57 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-const App = () => <h1>Hello World</h1>
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () => {
+    // save clicks of each button to own state
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
 
+    return (
+        <div>
+            <div>
+                <div>
+                    <h1>give feedback</h1>
+                </div>
+                <div>
+                    <button onClick={()=> setGood(good+1)}>Good</button>
+                    <button onClick={()=> setNeutral(neutral+1)}>Neutral</button>
+                    <button onClick={()=> setBad(bad+1)}>Bad</button>
+                </div>
+                <div>
+                    <h1>statistics</h1>
+                </div>
+                <div>
+                    <p>Good: {good}</p>
+                    <p>Neutral: {neutral}</p>
+                    <p>Bad: {bad}</p>
+                    <p>All: {good+bad+neutral}</p>
+                    <Average good={good} neutral={neutral} bad={bad}/>
+                    <Positive good={good} neutral={neutral} bad={bad}/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+const Positive = ({good, neutral, bad})=>{
+    if(good+neutral+bad <= 0){
+        return <p>Positive: No data</p>
+    }
+    return (
+        <p>Positive: {good*100/(good+bad+neutral)}%</p>
+    )
+}
+
+const Average = ({good, neutral, bad})=>{
+    if(good+neutral+bad <= 0){
+        return <p>Average: No data</p>
+    }
+    return (
+        <p>Average: {(good-bad)/(good+bad+neutral)}</p>
+    )
+}
+
+ReactDOM.render(<App />,
+    document.getElementById('root')
+)
