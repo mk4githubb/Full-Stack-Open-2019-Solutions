bee600ad3877c975c698850b59cdaa2eaa552d3a
Mk <kaushikmony17@gmail.com>
2019-12-20 19:54:15 +1100

    completed exercise 1.13

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -3,19 +3,32 @@ import Button from "./Button";
 
 const App = (props) => {
     const [selected, setSelected] = useState(0)
+    const [votes, setVotes] = useState(new Array(props.anecdotes.length).fill(0))
 
-    const handler = () => setSelected(RandomNumber(props.anecdotes.length))
+    const randomAnecdote = () => {
+        let num = RandomNumber(props.anecdotes.length);
+        setSelected(num);
+    }
+
+    const voteHandler = ()=>{
+        const copy = [...votes]
+        console.log(copy)
+        copy[selected] += 1
+        setVotes(copy)
+    }
 
     return (
         <div>
             <p>{props.anecdotes[selected]} </p>
-            <Button text={'Show Random Anecdote'} handler= {handler} />
+            <p>has {votes[selected]} votes </p>
+            <Button text={"Vote"} handler={voteHandler}/>
+            <Button text={'Show Random Anecdote'} handler= {randomAnecdote} />
         </div>
     )
 }
 
 const RandomNumber = (b) =>{
-    return Math.floor(Math.random()* (b+1))
+    return Math.floor(Math.random()* b)
 }
 
 export default  App
