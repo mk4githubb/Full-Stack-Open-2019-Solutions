7af107856c073b9a7ab524d1b3fbc638b716314f
Mk <kaushikmony17@gmail.com>
2019-12-30 21:55:54 +1100

    Completed exercise 3.4 and 3.5

--- part3/Phonebook/index.js
+++ part3/Phonebook/index.js
@@ -1,8 +1,10 @@
-const express = require('express')
+const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.json());
 
-const persons = [
+let persons = [
     {
         "name": "Dan Abramov",
         "number": "12-43-234345",
@@ -25,10 +27,17 @@ const persons = [
     }
 ]
 
+const getNextID = () => {
+    if(persons.length == 0){
+        return 0;
+    }
+    return Math.max(...persons.map(i => i.id)) + 1
+};
+
 app.get("/", (request, response) =>{
     response.send("<h1> Welcome to the PhoneBook!</h1> " +
                        "<p>Navigate to /api/persons tp get the list of all persons</p> ");
-})
+});
 
 app.get("/api/persons", (request, response ) =>{
     response.json(persons);
@@ -47,10 +56,44 @@ app.get("/api/persons/:id", (request, response)=> {
     }
 });
 
-app.get("/info", (request, response) => {
-    let toShow  = `<p> Phonebook has info for ${persons.length} people. </p>`+ `<p> ${new Date()}</p>`;
-    response.send(toShow)
+app.post("/api/persons/", (request, response)=>{
+    console.log(request.body);
+    const data = request.body;
+    if(!data.name || !data.number){
+        return response.status(400).json({
+            error: 'content missing'
+        });
+    }
+
+    const content = {
+        "name": data.name,
+        "number": data.number,
+        "id": getNextID()
+    }
+    persons = persons.concat(content);
+    response.status(200).json(content);
 })
 
-app.listen(3001)
+app.delete("/api/persons/:id", (request, response)=>{
+    let id = Number(request.params.id);
+    let found = persons.find(person => person.id === id);
+
+    if(found){
+        persons = persons.filter(person => person.id !== id);
+        console.log(persons)
+        return response.status(200).end();
+    }
+    else {
+        return response.status(404).end();
+    }
+
+});
+
+app.get("/info", (request, response) => {
+    let toShow  = `<p> Phonebook has info for ${persons.length} people. </p>`+ `<p> ${new Date()}</p>`;
+    return response.send(toShow)
+})
+
+const PORT = 3001;
+app.listen(PORT);
 console.log('listening on 3001');
