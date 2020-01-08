1063def3a8986443ccf6a7e69825b39ce874872b
Mk <kaushikmony17@gmail.com>
2020-01-09 10:04:24 +1100

    completed exercise 3.13 and 3.14

--- part3/commandlinedatabase/database.js
+++ part3/commandlinedatabase/database.js
@@ -1,35 +1,21 @@
 const mongoose = require('mongoose');
 
-if ( process.argv.length !== 5 ){
-    console.log('Please enter the password: ');
-    process.exit(0);
-}
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser : true})
+    .then(() => console.log("Connection Established"))
+    .catch(() => console.log("Connection Error"));
 
-const password = process.argv[2];
-const URL = `mongodb+srv://back_slash:${password}@cluster0-nqsyg.mongodb.net/phonebook?retryWrites=true`;
-
-mongoose.connect(URL, {useNewUrlParser : true});
-
-const schema = new mongoose.Schema({
+const Schema = new mongoose.Schema({
     name: String,
     number: String
 });
 
-const Row = mongoose.model('Row', schema);
+Schema.set('toJSON', {
+    transform: ( document , toReturn) => {
+    toReturn.id = document._id.toString();
+    delete toReturn._id
+    delete toReturn.__v
+}
 
-const newRow = new Row({
-    name: process.argv[3],
-    number: process.argv[4]
-});
+})
 
-newRow.save()
-    .then(() => console.log('Data saved'))
-    .catch(() => console.log('Error in saving data'))
-    .catch(error => console.log(error));
-
-
- const y = Row.find({})
-     .then(result => {
-         result.forEach( i => console.log(i.name) );
-        mongoose.connection.close();
-     });
+module.exports = mongoose.model('Row', Schema);
--- part3/commandlinedatabase/index.js
+++ part3/commandlinedatabase/index.js
@@ -1,71 +1,42 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
+app.use(express.static('build'));
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
+
+const Row = require('./database')
 const morgan = require('morgan');
 
 morgan.token('data', function(request){
     return JSON.stringify(request.body);
     }
 );
+
 app.use(morgan('::method :url :status :res[content-length] - :response-time ms :data'));
 
-app.use(express.static('build'));
-
-let persons = [
-    {
-        "name": "Dan Abramov",
-        "number": "12-43-234345",
-        "id": 1
-    },
-    {
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122",
-        "id": 2
-    },
-    {
-        "name": "Mom",
-        "number": "123",
-        "id": 3
-    },
-    {
-        "name": "mk",
-        "number": "mk",
-        "id": 4
-    }
-]
-
-const getNextID = () => {
-    if(persons.length == 0){
-        return 0;
-    }
-    return Math.max(...persons.map(i => i.id)) + 1
-};
-
-app.get("/", (request, response) =>{
-    response.send("<h1> Welcome to the PhoneBook!</h1> " +
-                       "<p>Navigate to /api/persons tp get the list of all persons</p> ");
-});
-
 app.get("/api/persons", (request, response ) =>{
-    response.json(persons);
+    Row.find({}).then( result => response.status(200).json(result.map(i => i.toJSON())));
 });
 
-app.get("/api/persons/:id", (request, response)=> {
-    let id = request.params.id;
-    let found = persons.find(person => person.id === Number(id));
 
-    console.log(found)
-    if(found){
-        response.json(found)
-    }
-    else{
-        response.status(404).end();
-    }
-});
+//
+//  NOT NEEDED, Not changed to accomodate DB
+//
+// app.get("/api/persons/:id", (request, response)=> {
+//     let id = request.params.id;
+//     let found = persons.find(person => person.id === (id));
+//
+//     console.log(found)
+//     if(found){
+//         response.json(found)
+//     }
+//     else{
+//         response.status(404).end();
+//     }
+// });
 
 app.post("/api/persons/", (request, response)=>{
-    console.log(request.body);
     const data = request.body;
     if(!data.name || !data.number){
         return response.status(400).json({
@@ -73,24 +44,31 @@ app.post("/api/persons/", (request, response)=>{
         });
     }
 
-    if(persons.some(i => i.name === data.name)){
+    var found = Row.find({name:data.name});
+
+    if(found.length > 1){
         return response.status(400).json({
             error: 'name must be unique'
         })
     }
 
-    const content = {
-        "name": data.name,
-        "number": data.number,
-        "id": getNextID()
-    }
-    persons = persons.concat(content);
-    response.status(200).json(content);
-})
+    const entry = new Row({
+        name: data.name,
+        number: data.number
+    });
+
+    entry.save()
+        .then(()=> response.status(200).json(entry.toJSON()))
+        .then(() => console.log('contact saved'))
+        .catch(() => response.status(500).end())
+        .catch(() => console.log('Error in saving contact'));
+});
 
 app.delete("/api/persons/:id", (request, response)=>{
-    let id = Number(request.params.id);
-    let found = persons.find(person => person.id === id);
+
+    const data = request.params;
+
+    var found = Row.find({id : data.id});
 
     if(found){
         persons = persons.filter(person => person.id !== id);
