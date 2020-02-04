2b1ededf4728a1e786ec9e8f36304989fc5ab718
Mk <kaushikmony17@gmail.com>
2020-02-04 15:18:45 +1100

    Completed  6.3

--- part6/redux-anecdotes-master/src/App.js
+++ part6/redux-anecdotes-master/src/App.js
@@ -1,33 +1,37 @@
 import React from 'react';
 
 const App = (props) => {
-  const anecdotes = props.store.getState()
+    const store = props.store;
+    const anecdotes = store.getState();
 
-  const vote = (id) => {
-    console.log('vote', id)
-  }
+    const vote = (id) => {
+        store.dispatch({
+            type: 'vote',
+            id: id
+        })
+    };
 
-  return (
-    <div>
-      <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
+    return (
+        <div>
+            <h2>Anecdotes</h2>
+            {anecdotes.map(anecdote =>
+                <div key={anecdote.id}>
+                    <div>
+                        {anecdote.content}
+                    </div>
+                    <div>
+                        has {anecdote.votes}
+                        <button onClick={() => vote(anecdote.id)}>vote</button>
+                    </div>
+                </div>
+            )}
+            <h2>create new</h2>
+            <form>
+                <div><input/></div>
+                <button>create</button>
+            </form>
         </div>
-      )}
-      <h2>create new</h2>
-      <form>
-        <div><input /></div>
-        <button>create</button>
-      </form>
-    </div>
-  )
-}
+    )
+};
 
 export default App
